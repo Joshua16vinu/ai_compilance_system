2e4f72625fd0ebae6b096e5fcff5cfec46a40d09
Joshua16vinu <142247991+Joshua16vinu@users.noreply.github.com>
2026-03-13 00:55:26 +0530

    resolved path configuation embedding

--- backend/embeddings/embedding_model.py
+++ backend/embeddings/embedding_model.py
@@ -1,8 +1,9 @@
+from pathlib import Path
 from sentence_transformers import SentenceTransformer
 
 _embedder = None
 BASE_DIR = Path(__file__).resolve().parents[2]
-MODEL_PATH = BASE_DIR / "models" / "all-MiniLM-L6-v2"
+MODEL_PATH = BASE_DIR / "backend" / "embeddings" / "models" / "all-MiniLM-L6-v2"
 def load_embedding_model():
     global _embedder
 
